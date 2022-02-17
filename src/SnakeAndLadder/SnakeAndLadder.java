e14c248b597275b1f9aa43ddf1d7641ec2199d0c
sandhiya97 <sandhiya.baburao@gmail.com>
2022-02-17 17:52:46 +0530

    Snake and ladder game played by initializing the start position 0

--- src/SnakeAndLadder/SnakeAndLadder.java
+++ src/SnakeAndLadder/SnakeAndLadder.java
@@ -1,7 +1,9 @@
 package SnakeAndLadder;
-
 public class SnakeAndLadder {
-	public static  void main(String[] args){
-		System.out.println("Welcome to Snake and Ladder Game");
-	}
+    public static void main(String[] args) {
+        //UC1 - Snake and Ladder game played with single player at start position 0
+        int position = 0;
+        System.out.println("Welcome to Snake and Ladder Game");
+        System.out.println("Initial Position is 0");
+    }
 }
